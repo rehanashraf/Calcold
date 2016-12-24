87cd3551b91dedf1471552eeb56b29767016693f
Rehan Ashraf <rashraf.cheema@gmail.com>
2016-12-23 23:36:43 -0800

    Added all the buttons and creted the Model Class

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Rehan Ashraf on 12/3/16.
+//  Copyright © 2016 Rehan Ashraf. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
 
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+     if   let digit = sender.currentTitle
+     {
+        let textCurrentlyInDisplay = display.text!
+        if userIsInTheMiddleOfTyping
+        {
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else
+        {
+            if digit == "."
+            {
+                
+            }
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+        }
+    }
+
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let mathematicalSymbol = sender.currentTitle{
+            if mathematicalSymbol == "π"{
+                displayValue = M_PI
+            }
+            else if mathematicalSymbol == "C"{
+                    displayValue = 0
+
+            }
+            else if mathematicalSymbol == "√" {
+                displayValue = sqrt(displayValue)
+            }
+            else if mathematicalSymbol == "±"{
+                displayValue = displayValue*(-1)
+            }
+        }
+        userIsInTheMiddleOfTyping = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
